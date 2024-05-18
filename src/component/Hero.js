fb9fae0cccb6f907d9991850d578a97fe66f48b8
Antonio <quadjovie.antonio@gmail.com>
2024-05-18 20:51:40 +0200

    re fix

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -5,7 +5,7 @@ function Hero () {
         <section className="bg-parallaxe">
             <h1>Antonio Quadjovie</h1>
             <p>Développeur Web - full stack</p>
-            <a href="../dl/QUADJOVIE-ANTONIO-5_compressed.pdf" download="QUADJOVIE-ANTONIO-5_compressed.pdf">
+            <a href="/dl/QUADJOVIE-ANTONIO-5_compressed.pdf" download="QUADJOVIE-ANTONIO-5_compressed.pdf">
                 <Button variant="primary">Télécharger mon cv</Button>
             </a>
         </section>
